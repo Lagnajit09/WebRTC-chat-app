cb7eb64c11a63d8ecaba16cbb9d7bb37d0639ae5
Lm09 <123.moharana@gmail.com>
2024-03-03 17:37:36 +0530

    Added initial frontend structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,51 @@
 import React from "react";
-import "./App.css";
+import { Typography, AppBar } from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
-  return <>App</>;
-}
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Options";
+import Notifications from "./components/Notifications";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "10px auto",
+    width: "50%",
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>{" "}
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
+};
 
 export default App;
